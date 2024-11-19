eb69746aad16bee86974393cc4529a6bec382601
Roger Abucejo Jr <rogerhapay@gmail.com>
2024-11-19 13:14:01 +0800

    reorganization

---
+++ src/components/icons/DOSTLogo.vue
@@ -0,0 +1,5 @@
+<template>
+    <div>
+        <img src="/src/assets//logo.png" alt="" srcset="">
+    </div>
+</template>
---
+++ src/fms/Auth/Login.vue
@@ -0,0 +1,9 @@
+<script setup>
+import DOSTLogo from '@/components/icons/DOSTLogo.vue'
+</script>
+
+<template>
+  <div>
+    <DOSTLogo/>
+  </div>
+</template>
