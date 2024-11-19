f726f8b450d063aca0ec69a87b41c46f474ba806
Roger Abucejo Jr <rogerhapay@gmail.com>
2024-11-19 18:01:44 +0800

    minor edits

--- src/fms/Auth/Login.vue
+++ src/fms/Auth/Login.vue
@@ -1,9 +1,14 @@
 <script setup>
 import DOSTLogo from '@/components/icons/DOSTLogo.vue'
+import PrimaryButton from '@/components/PrimaryButton.vue'
 </script>
 
 <template>
   <div>
     <DOSTLogo/>
+    <input type="text" name="username" id="username">
+    <input type="password" name="password" id="password">
+    <PrimaryButton class="bg-sky-500 rounded-lg px-4 py-2 w-full text">Login</PrimaryButton>
+
   </div>
 </template>
