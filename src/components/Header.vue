c07f02fb8323274193a03f20f45f034dbf071337
Roger Abucejo Jr <rogerhapay@gmail.com>
2024-12-05 17:24:39 +0800

    change logout icon

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -74,9 +74,10 @@ onMounted(() => {
               <p>Logout</p>
               <!-- Hide the text on small screens -->
               <!-- <span class="hidden sm:block">Logout</span> -->
-                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" class="w-5 h-5">
+                <!-- <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" class="w-5 h-5">
                   <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M17 16l4-4m0 0l-4-4m4 4H3"></path>
-                </svg>
+                </svg> -->
+                <RiLogoutBoxRLine/>
               </button>
             </div>
           </div>
