9ee6aef9781e0991ba1674a84762a2e7d056cde0
Roger Abucejo Jr <rogerhapay@gmail.com>
2025-02-28 10:00:33 +0800

    add vue-select component

--- src/main.js
+++ src/main.js
@@ -4,11 +4,12 @@ import router from './fms/Router/router.js'
 import { createApp } from 'vue'
 import App from './App.vue'
 import { createPinia } from 'pinia'
+import VueSelect from 'vue-select'
 
 const pinia = createPinia(); // ✅ Create a Pinia instance
 const app = createApp(App);
 
 app.use(pinia); // ✅ Register Pinia before using stores
 app.use(router);
-
+app.component("v-select", VueSelect); 
 app.mount('#app'); // ✅ Make sure this is the last call
