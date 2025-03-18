8ee8e5a8fd5dc613980c9bf572edebc2c8587cbc
Roger Abucejo Jr <rogerhapay@gmail.com>
2025-03-18 17:55:13 +0800

    add some props

--- src/components/Card.vue
+++ src/components/Card.vue
@@ -1,14 +1,23 @@
 <script setup>
 defineProps({
-    bg:{
-        type:String,
-        default: 'bg-blue-500'
-    }
+  bg: {
+    type: String,
+    default: "bg-blue-500",
+    required: false,
+  },
+  image: {
+    type: String,
+    default: "https://placehold.co/300x200", // Placeholder image
+    required: false,
+  },
 });
 </script>
 
 <template>
-        <div :class="`${bg} text-white p-4 rounded shadow`">
-            <slot></slot>
-        </div>
+  <div :class="`${bg} text-white p-4 rounded shadow`">
+    <!-- Image Placeholder -->
+    <img :src="image" alt="Image" class="w-full h-40 object-cover rounded-md mb-4" />
+
+    <slot></slot>
+  </div>
 </template>
