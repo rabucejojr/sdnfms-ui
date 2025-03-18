41a321579d76d424819649d156ce16981085133f
Roger Abucejo Jr <rogerhapay@gmail.com>
2025-03-18 17:55:57 +0800

    adjusted main dashboard layout

--- src/MainDashboard.vue
+++ src/MainDashboard.vue
@@ -1,7 +1,11 @@
 <script setup>
 import Header from "@/components/Header.vue";
+import Card from "./components/Card.vue";
+import PressRelease from "./dashboard/PressRelease.vue";
 </script>
 <template>
   <Header />
-  <div>main-dashboard</div>
+  <div class="">
+    <PressRelease />
+  </div>
 </template>
