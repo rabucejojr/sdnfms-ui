a0eec7d111e9561f0f5863a74e4c3c39f2f75d21
Roger Abucejo Jr <rogerhapay@gmail.com>
2025-03-19 10:05:08 +0800

    remove image

--- src/components/Card.vue
+++ src/components/Card.vue
@@ -5,19 +5,11 @@ defineProps({
     default: "bg-blue-500",
     required: false,
   },
-  image: {
-    type: String,
-    default: "https://placehold.co/300x200", // Placeholder image
-    required: false,
-  },
 });
 </script>
 
 <template>
   <div :class="`${bg} text-white p-4 rounded shadow`">
-    <!-- Image Placeholder -->
-    <img :src="image" alt="Image" class="w-full h-40 object-cover rounded-md mb-4" />
-
     <slot></slot>
   </div>
 </template>
